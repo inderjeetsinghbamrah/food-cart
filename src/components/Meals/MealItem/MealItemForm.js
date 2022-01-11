f9f742231adb25ab6ad4ee1b2effb318f2c4cf65
Inderjeet Singh Bamrah <inderjeet.s.bamrah@gmail.com>
2022-01-11 16:37:57 +0530

    working on modal overlay

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 const MealItemForm = (props) => {
   return (
     <form className={classes.MealItemForm}>
-      <button>+Add </button>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button className={classes.button}>+Add </button>
     </form>
   );
 };
